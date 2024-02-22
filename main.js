21707774215b998afd00a83f7706113ad018371c
yamanalrfai <155045988+yamanalrfai@users.noreply.github.com>
2024-02-22 20:42:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//task
+let arr1=[1,2,3,4,5];
+let arr2=arr1.map(arr1=>{
+    return 2*arr1
+});
+let arr3=[];
+for(let i=0;i<5;i++)
+{
+    arr3[i]=i;
+}
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+//
